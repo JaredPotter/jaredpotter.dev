8dd051317bdcc55ca650278cf5ded753aab8a585
Larry512 <jaredpotter1@gmail.com>
2019-03-25 23:53:01 -0600

    More changes.

---
+++ build/precache-manifest.237ac0dede6bdc3c4a7d273ab8b22844.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "dbb922d3697f8fba81cf",
+    "url": "/static/js/main.b128a5aa.chunk.js"
+  },
+  {
+    "revision": "64af41677e84e4c1c9ad",
+    "url": "/static/js/2.232b077b.chunk.js"
+  },
+  {
+    "revision": "dbb922d3697f8fba81cf",
+    "url": "/static/css/main.8995bf03.chunk.css"
+  },
+  {
+    "revision": "adffa06ebe94bd31eb1154b922d390a7",
+    "url": "/index.html"
+  }
+];
