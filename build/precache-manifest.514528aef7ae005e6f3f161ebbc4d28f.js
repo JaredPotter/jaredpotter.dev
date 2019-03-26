35aa3d2299824601b994dd203643dadb67448d66
Larry512 <jaredpotter1@gmail.com>
2019-03-25 22:03:51 -0600

    Removed /build from gitignore.

---
+++ build/precache-manifest.514528aef7ae005e6f3f161ebbc4d28f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "791c25c9e25d34ad4b69",
+    "url": "/static/js/main.a65f3423.chunk.js"
+  },
+  {
+    "revision": "ccdf85a272b151ebd001",
+    "url": "/static/js/2.7b7f7f25.chunk.js"
+  },
+  {
+    "revision": "791c25c9e25d34ad4b69",
+    "url": "/static/css/main.e449423a.chunk.css"
+  },
+  {
+    "revision": "de7db27e919513fc2775f046ede4ba95",
+    "url": "/index.html"
+  }
+];
